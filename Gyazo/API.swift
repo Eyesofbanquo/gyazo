7e06a3f73ac365c93ec40d33012637a949f86870
Markim Shaw <ms79723@gmail.com>
2020-04-22 01:51:31 -0500

    Initial

---
+++ Gyazo/API.swift
@@ -0,0 +1,17 @@
+//
+//  API.swift
+//  Gyazo
+//
+//  Created by Markim Shaw on 4/22/20.
+//  Copyright © 2020 Markim Shaw. All rights reserved.
+//
+
+import Foundation
+
+struct API {
+  static var url = "https://api.gyazo.com"
+  static var clientID = "695a8224edc18ed8c21890ae61740b1d3573239c14c36fab40dcb1e9d3acb73f"
+  static var clientSecret = "a5095320f68121a96c132e0d9b414ba393260986b90393c0d63c04f71d617060"
+  static var callbackURL = "http://donderapps.com"
+  static var accessToken = "5e39584f639ecf51120cc3be7e865938b261b23c64393b7c7e0ac1404779a422"
+}
