23ebd6b757af1ec8786fa64c8dae282de259285a
Markim Shaw <ms79723@gmail.com>
2020-08-24 00:18:39 -0500

    Add finishing touches to Picker

--- Gyazo/SwiftUI/ImagePicker.swift
+++ Gyazo/SwiftUI/ImagePicker.swift
@@ -23,6 +23,8 @@ struct ImagePicker: UIViewControllerRepresentable {
   func makeUIViewController(context: Context) -> UIImagePickerController {
     let picker = UIImagePickerController()
     picker.delegate = context.coordinator
+    picker.sourceType = .photoLibrary
+    //picker.sourceType = .camera
     return picker
   }
   
@@ -48,5 +50,36 @@ extension ImagePicker {
       
       parent.presentationMode.wrappedValue.dismiss()
     }
+  }
+}
+
+// MARK: - This is for quick testing -
+
+struct ImagePickerView: View {
+  
+  @State private var imageView: Image?
+  @State private var image: UIImage?
+  @State var showingImagePicker = false
+  
+  var body: some View {
+    VStack {
+      imageView?.resizable().aspectRatio(contentMode: .fit)
+      Button("Tap to present picker") {
+        self.showingImagePicker = true
+      }
+    }.sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+      ImagePicker(image: self.$image)
+    }
+  }
+  
+  func loadImage() {
+    guard let image = self.image else { return }
+    imageView = Image(uiImage: image)
+  }
+}
+
+struct ImagePicker_Previews: PreviewProvider {
+  static var previews: some View {
+    ImagePickerView()
   }
 }
