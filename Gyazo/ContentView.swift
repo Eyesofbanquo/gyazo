26a0f15c9737b52f49a128bd55538c9c4204c417
Markim Shaw <ms79723@gmail.com>
2020-08-24 00:44:22 -0500

    Add Image Icon to ContentView

--- Gyazo/ContentView.swift
+++ Gyazo/ContentView.swift
@@ -22,63 +22,74 @@ struct ContentView: View {
   
   @State var searchText: String = ""
   
+  @State var showingImagePicker = false
+  
+  @State var selectedImage: UIImage?
+  
   var body: some View {
     
     
     NavigationView {
-      VStack {
-        ScrollView {
-          //          GeometryReader { geo in
-          //            Image("gyazo-image")
-          //              .resizable()
-          //              .aspectRatio(contentMode: .fill)
-          //              .offset(y: geo.frame(in: .global).minY > 0 ? -geo.frame(in: .global).minY : 0)
-          //              .frame(width: UIScreen.main.bounds.width,
-          //                     height: geo.frame(in: .global).minY > 0 ? geo.frame(in: .global).minY + 300 : 300)
-          //          }
-          //          .frame(height: 300)
-          
-          SearchBar(text: $searchText)
-          ForEach(posts.filter { post in
-            if (searchText.isEmpty) {
-              return true
-            } else {
-              let appropriateMLResponses = self.vision.classifications[post.metadata?.title ?? ""]
-              let bestResponse = appropriateMLResponses?.first
-              let searchTextContainsResponse = (bestResponse?.1.lowercased() ?? "").contains(self.searchText.lowercased())
-              return (post.metadata?.app?.contains(self.searchText)) == true || searchTextContainsResponse == true
-            }
-          }, id: \.self) { post in
-            Group {
-              if post.urlString.isEmpty == false {
-                DashboardCell(post: post, placeholder: Text("Loading"))
-                  .padding(.horizontal, 8.0)
+      ZStack(alignment: .bottomTrailing) {
+        VStack {
+          ScrollView {
+            //          GeometryReader { geo in
+            //            Image("gyazo-image")
+            //              .resizable()
+            //              .aspectRatio(contentMode: .fill)
+            //              .offset(y: geo.frame(in: .global).minY > 0 ? -geo.frame(in: .global).minY : 0)
+            //              .frame(width: UIScreen.main.bounds.width,
+            //                     height: geo.frame(in: .global).minY > 0 ? geo.frame(in: .global).minY + 300 : 300)
+            //          }
+            //          .frame(height: 300)
+            
+            SearchBar(text: $searchText)
+            ForEach(posts.filter { post in
+              if (searchText.isEmpty) {
+                return true
+              } else {
+                let appropriateMLResponses = self.vision.classifications[post.metadata?.title ?? ""]
+                let bestResponse = appropriateMLResponses?.first
+                let searchTextContainsResponse = (bestResponse?.1.lowercased() ?? "").contains(self.searchText.lowercased())
+                return (post.metadata?.app?.contains(self.searchText)) == true || searchTextContainsResponse == true
+              }
+            }, id: \.self) { post in
+              Group {
+                if post.urlString.isEmpty == false {
+                  DashboardCell(post: post, placeholder: Text("Loading"))
+                    .padding(.horizontal, 8.0)
+                }
+              }
+            }.onReceive(request.request(endpoint: "images")) { posts in
+              if let posts = posts {
+                self.posts = posts
               }
             }
-          }.onReceive(request.request(endpoint: "images")) { posts in
-            if let posts = posts {
-              self.posts = posts
-            }
+              
+            .navigationBarTitle(Text("Gyazo"))
+            .navigationBarItems(
+              trailing: Button(action: {
+                // - Launch Profile Modal
+              }){
+                Image(systemName: "person.circle").font(.headline)
+            })
           }
-            
-          .navigationBarTitle(Text("Gyazo"))
-          .navigationBarItems(
-            leading:
-            Button(action: {
-              // - Launch Search
-            }){
-              Image(systemName: "magnifyingglass")}
-            ,
-            trailing: Button(action: {
-              // - Launch Profile Modal
-            }){
-              Image(systemName: "person.circle")
-          })
         }
-      }
-    }
- 
-  }
+        Image(systemName: "camera")
+          .padding()
+          .background(Circle().foregroundColor(Color(.systemTeal)))
+          .offset(x: -16, y: -16)
+          .shadow(color: Color(.systemTeal), radius: 8.0, x: 0, y: 0)
+          .onTapGesture {
+            self.showingImagePicker = true
+        }
+      } // stack
+        .sheet(isPresented: $showingImagePicker, content: {
+          ImagePicker(image: self.$selectedImage)
+        })
+    } // nav view
+    
+  } // body
   
   
 }
