229df03a876ad5270a473c0c158747b9aa19f3df
Eyesofbanquo <djsalinger67@live.com>
2020-08-25 00:39:17 -0500

    Add clipboard suppor (#15)
    - Add clipboard support
    - Add a rudimentary system for modal presentation
    
    Co-authored-by: Markim Shaw <ms79723@gmail.com>

--- Gyazo/ContentView.swift
+++ Gyazo/ContentView.swift
@@ -15,9 +15,7 @@ struct ContentView: View {
   @ObservedObject var request: NetworkRequest<[Drop]> = NetworkRequest<[Drop]>()
   
   @State var posts: [Drop] = []
-  
-  @Environment(\.imageCache) var cache: ImageCacheable
-  
+    
   @Environment(\.vision) var vision: Vision
   
   @State var searchText: String = ""
@@ -26,71 +24,105 @@ struct ContentView: View {
   
   @State var selectedImage: UIImage?
   
+  @State var pasteboardImage: UIImage?
+  
   var body: some View {
     
-    
-    NavigationView {
-      ZStack(alignment: .bottomTrailing) {
-        VStack {
-          ScrollView {
-            //          GeometryReader { geo in
-            //            Image("gyazo-image")
-            //              .resizable()
-            //              .aspectRatio(contentMode: .fill)
-            //              .offset(y: geo.frame(in: .global).minY > 0 ? -geo.frame(in: .global).minY : 0)
-            //              .frame(width: UIScreen.main.bounds.width,
-            //                     height: geo.frame(in: .global).minY > 0 ? geo.frame(in: .global).minY + 300 : 300)
-            //          }
-            //          .frame(height: 300)
-            
-            SearchBar(text: $searchText)
-            ForEach(posts.filter { post in
-              if (searchText.isEmpty) {
-                return true
-              } else {
-                let appropriateMLResponses = self.vision.classifications[post.metadata?.title ?? ""]
-                let bestResponse = appropriateMLResponses?.first
-                let searchTextContainsResponse = (bestResponse?.1.lowercased() ?? "").contains(self.searchText.lowercased())
-                return (post.metadata?.app?.contains(self.searchText)) == true || searchTextContainsResponse == true
-              }
-            }, id: \.self) { post in
-              Group {
-                if post.urlString.isEmpty == false {
-                  DashboardCell(post: post, placeholder: Text("Loading"))
-                    .padding(.horizontal, 8.0)
+    ZStack(alignment: .center) {
+      NavigationView {
+        ZStack(alignment: .bottomTrailing) {
+          VStack {
+            ScrollView {
+              //          GeometryReader { geo in
+              //            Image("gyazo-image")
+              //              .resizable()
+              //              .aspectRatio(contentMode: .fill)
+              //              .offset(y: geo.frame(in: .global).minY > 0 ? -geo.frame(in: .global).minY : 0)
+              //              .frame(width: UIScreen.main.bounds.width,
+              //                     height: geo.frame(in: .global).minY > 0 ? geo.frame(in: .global).minY + 300 : 300)
+              //          }
+              //          .frame(height: 300)
+              
+              SearchBar(text: $searchText)
+              ForEach(posts.filter { post in
+                if (searchText.isEmpty) {
+                  return true
+                } else {
+                  let appropriateMLResponses = self.vision.classifications[post.metadata?.title ?? ""]
+                  let bestResponse = appropriateMLResponses?.first
+                  let searchTextContainsResponse = (bestResponse?.1.lowercased() ?? "").contains(self.searchText.lowercased())
+                  return (post.metadata?.app?.contains(self.searchText)) == true || searchTextContainsResponse == true
+                }
+              }, id: \.self) { post in
+                Group {
+                  if post.urlString.isEmpty == false {
+                    DashboardCell(post: post, placeholder: Text("Loading"))
+                      .padding(.horizontal, 8.0)
+                  }
+                }
+              }.onReceive(request.request(endpoint: "images")) { posts in
+                if let posts = posts {
+                  self.posts = posts
                 }
               }
-            }.onReceive(request.request(endpoint: "images")) { posts in
-              if let posts = posts {
-                self.posts = posts
-              }
+                
+              .navigationBarTitle(Text("Gyazo"))
+              .navigationBarItems(
+                trailing: Button(action: {
+                  // - Launch Profile Modal
+                  self.readFromPasteboard()
+                }){
+                  Image(systemName: "person.circle").font(.headline)
+              })
             }
-              
-            .navigationBarTitle(Text("Gyazo"))
-            .navigationBarItems(
-              trailing: Button(action: {
-                // - Launch Profile Modal
-              }){
-                Image(systemName: "person.circle").font(.headline)
-            })
           }
-        }
-        Image(systemName: "camera")
-          .padding()
-          .background(Circle().foregroundColor(Color(.systemTeal)))
-          .offset(x: -16, y: -16)
-          .shadow(color: Color(.systemTeal), radius: 8.0, x: 0, y: 0)
-          .onTapGesture {
-            self.showingImagePicker = true
-        }
-      } // stack
-        .sheet(isPresented: $showingImagePicker, content: {
-          ImagePicker(image: self.$selectedImage)
-        })
-    } // nav view
+          Image(systemName: "camera")
+            .padding()
+            .background(Circle().foregroundColor(Color(.systemTeal)))
+            .offset(x: -16, y: -16)
+            .shadow(color: Color(.systemTeal), radius: 8.0, x: 0, y: 0)
+            .onTapGesture {
+              self.showingImagePicker = true
+          }
+        } // stack
+          .sheet(isPresented: $showingImagePicker, content: {
+            ImagePicker(image: self.$selectedImage)
+          })
+        
+      } // nav view
+      
+      if self.pasteboardImage != nil {
+        ZStack(alignment: .center) {
+          //        Color.red
+          VStack {
+            //          Image("gyazo-image")
+            pasteboardImageView?
+              .resizable()
+              .padding(.horizontal)
+              .aspectRatio(contentMode: .fit)
+              .onTapGesture {
+                self.pasteboardImage = nil
+            }
+            Text("Image classification")
+          }
+        } // inner z-stack
+      }
+
+    }// outer z-stack
     
   } // body
   
+  var pasteboardImageView: Image? {
+    guard let image = pasteboardImage else { return nil }
+    
+    return Image.init(uiImage: image)
+  }
+  private func readFromPasteboard() {
+    let pasteboard = UIPasteboard.general
+    guard let pasteboardImage = pasteboard.image else { return }
+    
+    self.pasteboardImage = pasteboardImage
+  }
   
 }
 
