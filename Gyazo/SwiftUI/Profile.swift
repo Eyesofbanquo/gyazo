8591da3d6c760e1ea3d8b69176b75edb21740001
Markim Shaw <ms79723@gmail.com>
2020-08-25 00:48:11 -0500

    Add profile view file

---
+++ Gyazo/SwiftUI/Profile.swift
@@ -0,0 +1,24 @@
+//
+//  Profile.swift
+//  Gyazo
+//
+//  Created by Markim Shaw on 8/25/20.
+//  Copyright © 2020 Markim Shaw. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct Profile: View {
+  
+  
+  var body: some View {
+    Text("")
+  }
+}
+
+struct Profile_Providers: PreviewProvider {
+  static var previews: some View {
+    Profile()
+  }
+}
