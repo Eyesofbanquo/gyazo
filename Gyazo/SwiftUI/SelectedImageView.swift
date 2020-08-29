411bd0af876c495aac6ad41adbe4d70167e6e30c
Markim Shaw <ms79723@gmail.com>
2020-08-29 14:22:02 -0500

    Refactor SeelctedImageView

--- Gyazo/SwiftUI/SelectedImageView.swift
+++ Gyazo/SwiftUI/SelectedImageView.swift
@@ -16,54 +16,109 @@ struct SelectedImageView: View {
   
   @State private var performedAction: Bool = false
   
-  private var action: (() -> Void)?
+  @State private var presentShareController: Bool = false
+  
+  var action: (() -> Void)?
   
   @Binding var presentSelectedImageView: Bool
   
   var body: some View {
     ZStack(alignment: .center) {
-      Color.black
-        .opacity(0.05)
-        .background(BlurView(style: .systemMaterial))
+      Background
       VStack {
-        unwrappedImage?
-          .resizable()
-          .aspectRatio(contentMode: .fit)
-        Text("\(self.performedAction ? actionText.performedAction : actionText.default)")
-          .padding()
-          .foregroundColor(Color(.systemBackground))
-          .background(self.performedAction ? Color(.label) : Color(.label))
-          .clipShape(Capsule())
-          .onTapGesture {
-            withAnimation {
-              self.performedAction = true
-            }
-            self.action?()
-          }
+        DisplayableImage
+        HStack(alignment: .center, spacing: 16.0) {
+          ActionButton
+          ShareButton
+        }
       }
     }
     .edgesIgnoringSafeArea(.all)
     .transition(.scale)
     .onTapGesture {
-      self.$presentSelectedImageView.wrappedValue = false
-      self.performedAction = false
+      self.dismissAndReset()
     }
   }
+}
+
+struct SelectedImageView_Previews: PreviewProvider {
+  static var present: Bool = false
   
+  static var presentBinding = Binding<Bool>(
+    get: {
+      return present
+    },
+    set: {
+      present = $0
+    })
+  
+  static var previews: some View {
+    
+    Group {
+      SelectedImageView(uiimage: UIImage(named: "gyazo-image"),
+                        actionText: (performedAction: "Cancelled",
+                                     default: "Tap on this to cancel"),
+                        presentSelectedImageView: presentBinding)
+        .preferredColorScheme(.dark)
+      SelectedImageView(uiimage: UIImage(named: "gyazo-image"),
+                        actionText: (performedAction: "Cancelled",
+                                     default: "Tap on this to cancel"),
+                        presentSelectedImageView: presentBinding)
+        .preferredColorScheme(.light)
+    }
+    
+  }
+}
+
+// MARK: - Helpers -
+extension SelectedImageView {
   var unwrappedImage: Image? {
     guard let image = uiimage else { return nil }
     
     return Image(uiImage: image)
   }
-}
-
-struct SelectedImageView_Previews: PreviewProvider {
-  static var previews: some View {
-    Group {
-      SelectedImageView()
-        .preferredColorScheme(.dark)
-      SelectedImageView()
-        .preferredColorScheme(.light)
+  
+  private var DisplayableImage: some View {
+    unwrappedImage?
+      .resizable()
+      .aspectRatio(contentMode: .fit)
+  }
+  
+  private var ActionButton: some View {
+    Button(action: {
+      withAnimation {
+        self.performedAction = true
+      }
+      self.action?()
+    }) {
+      Text("\(self.performedAction ? actionText.performedAction : actionText.default)")
+        .padding()
+        .foregroundColor(Color(.systemBackground))
+        .background(self.performedAction ? Color(.label) : Color(.label))
+        .clipShape(Capsule())
     }
+  }
+  
+  private var ShareButton: some View {
+    Button(action: {
+      self.presentShareController.toggle()
+    }) {
+      Image(systemName: "square.and.arrow.up")
+        .font(.title)
+    }.buttonStyle(PlainButtonStyle())
+    .sheet(isPresented: $presentShareController, content: {
+      EmptyView()
+    })
+  }
+  
+  private var Background: some View {
+    Color.black
+      .opacity(0.05)
+      .background(BlurView(style: .systemMaterial))
+  }
+  
+  private func dismissAndReset() {
+    self.$presentSelectedImageView.wrappedValue = false
+    self.performedAction = false
   }
 }
